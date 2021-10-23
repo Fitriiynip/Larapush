1c6579292fa589104887138432e5cbfd3d914e8b
fitriiynip <fitriynip6@gmail.com>
2021-10-23 16:33:49 -0400

    Ressult Gabut wkwk

---
+++ resources/views/layouts/partials/content.blade.php
@@ -0,0 +1,12 @@
+<html>
+
+<head>
+</head>
+
+<body>
+<center><marquee direction="up" scrollamount="2" align="center" behavior="alternate" width="90%">
+<marquee direction="right" behavior="alternate"> Haiiiii <br>
+                                                Are you having great day? <br>
+                                                i always hope your have a  nice day </marquee></marquee></center>
+</body>
+</html>
